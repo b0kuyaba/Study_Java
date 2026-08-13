0cb789d5d5725b2aa95ee68cdcf2bfd9cec9b283
b0kuyaba <e10yc1122@gmail.com>
2026-08-13 22:11:30 +0900

    자료형 공부

---
+++ src/OperationEx1.java
@@ -0,0 +1,10 @@
+public class OperationEx1 {
+
+    public static void main(String[] args) {
+
+        int score = 92;
+
+        boolean result = 80 <= score && score <= 100;
+        System.out.println(result);
+    }
+}
