da3cfab3571ab42f14adb7c8cbf2c66e99345950
b0kuyaba <e10yc1122@gmail.com>
2026-09-16 19:22:01 +0900

    Q11 풀이

---
+++ src/practice_1/Q11.java
@@ -0,0 +1,11 @@
+package practice_1;
+
+public class Q11 {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        int a = 12;
+
+        final double ROUND = a * a * pi;
+        System.out.println("반지름이 12인 원의 넓이는 " + ROUND);
+    }
+}
